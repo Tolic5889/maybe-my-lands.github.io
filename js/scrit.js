af9d4db960d460bb167b5cb6e1f50dedaa749cd1
Tolic5889 <126387432+Tolic5889@users.noreply.github.com>
2023-02-26 12:39:16 +0200

    Create scrit.js

---
+++ js/scrit.js
@@ -0,0 +1,53 @@
+function open_photo(photo_1) {
+    document.getElementById("big-photo_1").innerHTML =
+    ("<img onclick='close_photo()' style='position: absolute; width: 1020px;height: 603px;z-index: 10000;' src='" + photo_1 + "'>")
+}
+  
+function close_photo() {
+    document.getElementById("big-photo_1").innerHTML = ""
+}
+
+function open_photo(photo_2) {
+    document.getElementById("big-photo_2").innerHTML =
+    ("<img onclick='close_photo()' style='position: absolute;z-index: 10000;width: 1020px;height: 603px;' src='" + photo_2 + "'>")
+}
+  
+function close_photo() {
+    document.getElementById("big-photo_2").innerHTML = ""
+}
+
+function open_photo(photo_3) {
+    document.getElementById("big-photo_3").innerHTML =
+    ("<img onclick='close_photo()' style='position: absolute;z-index: 10000;width: 1020px;height: 603px;rignt:1000px;' src='" + photo_3 + "'>")
+}
+  
+function close_photo() {
+    document.getElementById("big-photo_3").innerHTML = ""
+}
+
+function open_photo(photo_4) {
+    document.getElementById("big-photo_4").innerHTML =
+    ("<img onclick='close_photo()' style='position: absolute;z-index: 10000;width: 1020px;' src='" + photo_4 + "'>")
+}
+  
+function close_photo() {
+    document.getElementById("big-photo_4").innerHTML = ""
+}
+
+function open_photo(photo_5) {
+    document.getElementById("big-photo_5").innerHTML =
+    ("<img onclick='close_photo()' style='position: absolute;z-index: 10000;width: 1020px;height: 603px;' src='" + photo_5 + "'>")
+}
+  
+function close_photo() {
+    document.getElementById("big-photo_5").innerHTML = ""
+}
+
+function open_photo(photo_6) {
+    document.getElementById("big-photo_6").innerHTML =
+    ("<img onclick='close_photo()' style='position: absolute;z-index: 100;width: 850px;height: 835px;' src='" + photo_6 + "'>")
+}
+  
+function close_photo() {
+    document.getElementById("big-photo_6").innerHTML = ""
+}
